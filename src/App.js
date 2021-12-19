c57bca9ca6cbfc2edc7c48807d0a9a8bd6c4793c
hwryeol <hwwy0425@gmail.com>
2021-12-19 22:01:16 +0900

    delete file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component{
+  // mounting,update,unmounting
+  //constructor(): 클래스 인스턴스 객체를 생성하고 초기화하는 매소드
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  componentDidMount(){
+    setTimeout( ()=> {
+      this.setState({isLoading:false});
+    },6000);
+  }
+  render(){
+    const {isLoading} = this.state;
+    return (<div>{isLoading ? "Loading": "We are ready"}</div>)
+  }
+
 }
 
 export default App;
+
+/*
+food를 그냥 넘기면 food.prop.name 이런식으로 써야하는데,
+오브젝트로 넘기면 prop에다가 오브젝트 내용을 담아서 넣는거 같다.
+*/
